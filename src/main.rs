2b9b7894d1a39f6ca3fa286c640fe294eb228a22
Greg Manning <1396183+tocklime@users.noreply.github.com>
2019-12-26 08:03:25 +0000

    input and some tidying

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,66 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::fs::File;
 use std::io;
-use std::io::Read;
-use std::convert::TryInto;
+use std::io::{Read, stdout, Write, stdin};
+use std::convert::{TryFrom, TryInto};
 use itertools::Itertools;
+use num_enum::TryFromPrimitive;
+
+#[derive(Debug, TryFromPrimitive)]
+#[repr(u16)]
+pub enum Op {
+    Halt = 0,
+    Set,
+    Push,
+    Pop,
+    Eq,
+    Gt,
+    Jmp,
+    Jt,
+    Jf,
+    Add,
+    Mult,
+    Mod,
+    And,
+    Or,
+    Not,
+    Rmem,
+    Wmem,
+    Call,
+    Ret,
+    Out,
+    In,
+    Nop,
+}
 
 #[derive(Debug)]
 pub struct Vm {
     memory: HashMap<u16, u16>,
-    registers: [u16; 8],
     stack: Vec<u16>,
     instruction_pointer: u16,
     running: bool,
     dumping: bool,
+    input: Vec<char>,
 }
 
 impl Vm {
     pub fn new(program: &[u16]) -> Self {
         Vm {
             memory: program.iter().enumerate().map(|(i, x)| (i.try_into().unwrap(), *x)).collect(),
-            registers: [0_u16; 8],
             stack: Vec::new(),
             instruction_pointer: 0,
             running: true,
             dumping: false,
+            input: Vec::new(),
         }
     }
     fn fetch_read(&mut self) -> u16 {
         let i = self.fetch_set();
         if i >= 32768 {
-            self.registers[(i - 32768) as usize]
+            self.get(i)
         } else { i }
     }
-    fn log(&self, s:String) {
+    fn log(&self, s: String) {
         if self.dumping {
             print!("{}", s);
         }
@@ -40,24 +68,14 @@ impl Vm {
     fn fetch_set(&mut self) -> u16 {
         let i = self.memory[&self.instruction_pointer];
         self.instruction_pointer += 1;
-        self.log(format!("{} ", i));
+        //self.log(format!("{} ", i));
         i
     }
     fn set(&mut self, address: u16, value: u16) {
-        if address >= 32768 {
-            self.log(format!(" R{}={} ", address - 32768, value));
-            self.registers[(address - 32768) as usize] = value
-        } else {
-            self.log(format!("M{}={}", address, value));
-            self.memory.insert(address, value);
-        }
+        self.memory.insert(address, value);
     }
     fn get(&mut self, address: u16) -> u16 {
-        if address >= 32768 {
-            self.registers[(address - 32768) as usize]
-        } else {
-            *self.memory.entry(address).or_default()
-        }
+        *self.memory.entry(address).or_default()
     }
     fn binop<FN>(&mut self, f: FN)
         where FN: Fn(u16, u16) -> u16
@@ -66,82 +84,92 @@ impl Vm {
         self.set(a, (f(b, c)) % 32768);
     }
     fn step(&mut self) {
-        self.log(format!("@{} ",self.instruction_pointer));
-        let op = self.fetch_read();
+        //self.log(format!("@{} ",self.instruction_pointer));
+        let op: Op = self.fetch_read().try_into().expect("Unknown op code");
         match op {
-            0 => self.running = false,
-            1 => {
+            Op::Halt => self.running = false,
+            Op::Set => {
                 let a = self.fetch_set();
                 let b = self.fetch_read();
                 self.set(a, b);
             }
-            2 => {
+            Op::Push => {
                 let a = self.fetch_read();
                 self.stack.push(a);
             }
-            3 => {
+            Op::Pop => {
                 let a = self.fetch_set();
                 let v = self.stack.pop().expect("Empty stack!");
                 self.set(a, v);
             }
-            4 => self.binop(|a, b| (a == b).into()),
-            5 => self.binop(|a, b| (a > b).into()),
-            6 => self.instruction_pointer = self.fetch_read(),
-            7 => {
+            Op::Eq => self.binop(|a, b| (a == b).into()),
+            Op::Gt => self.binop(|a, b| (a > b).into()),
+            Op::Jmp => self.instruction_pointer = self.fetch_read(),
+            Op::Jt => {
                 let (a, b) = (self.fetch_read(), self.fetch_read());
                 if a != 0 {
                     self.instruction_pointer = b;
                 }
             }
-            8 => {
+            Op::Jf => {
                 let (a, b) = (self.fetch_read(), self.fetch_read());
                 if a == 0 {
                     self.instruction_pointer = b;
                 }
             }
-            9 => self.binop(|a, b| a + b),
-            10 => self.binop(|a, b| a.wrapping_mul(b)),
-            11 => self.binop(|a, b| a % b),
-            12 => self.binop(|a, b| a & b),
-            13 => self.binop(|a, b| a | b),
-            14 => {
+            Op::Add => self.binop(|a, b| a + b),
+            Op::Mult => self.binop(|a, b| a.wrapping_mul(b)),
+            Op::Mod => self.binop(|a, b| a % b),
+            Op::And => self.binop(|a, b| a & b),
+            Op::Or => self.binop(|a, b| a | b),
+            Op::Not => {
                 let a = self.fetch_set();
                 let b = self.fetch_read();
                 self.set(a, (!b) % 32768);
             }
-            15 => {
+            Op::Rmem => {
                 let a = self.fetch_set();
                 let ab = self.fetch_read();
                 let b = self.get(ab);
                 self.set(a, b);
             }
-            16 => {
+            Op::Wmem => {
                 let a = self.fetch_read();
                 let b = self.fetch_read();
                 self.set(a, b);
             }
-            17 => {
+            Op::Call => {
                 let a = self.fetch_read();
                 self.stack.push(self.instruction_pointer);
                 self.instruction_pointer = a;
             }
-            18 => {
+            Op::Ret => {
                 if self.stack.is_empty()
                 { self.running = false; } else {
                     self.instruction_pointer = self.stack.pop().expect("Empty stack after check");
                 }
             }
-            19 => {
+            Op::Out => {
                 let ch: u16 = self.fetch_read();
                 let ch: char = std::char::from_u32(ch.into()).expect("Invalid char");
                 print!("{}", ch);
             }
-            21 => (), // NoOp
-            _ => panic!("Op not implemented: {}@{}", op, self.instruction_pointer - 1),
+            Op::In => {
+                if self.input.is_empty() {
+                    let _ = stdout().flush();
+                    let mut s = String::new();
+                    stdin().read_line(&mut s).expect("Bad input");
+                    self.input = s.chars().filter(|x| x != &'\r').rev().collect();
+                }
+                let a = self.fetch_set();
+                let i = self.input.pop().unwrap() as u16;
+                self.set(a,i);
+            }
+            Op::Nop => (), // NoOp
         }
     }
-    pub fn dump(&self){
-        println!("\nVM:\n R:{:?}\n St:{:?}\n IP: {}",self.registers,self.stack,self.instruction_pointer);
+    pub fn dump(&self) {
+        println!("\nVM:\n St:{:?}\n IP: {}", self.stack, self.instruction_pointer);
     }
     pub fn run(&mut self) {
         while self.running {
